7d7d56e77d262cce7238fac6fe17e0af706ca90e
SkunkHeadedPunk <jack.woolery.94@gmail.com>
2022-02-17 11:02:02 -0700

    Ready for IDR Demo - completes stage 1-2, gives control distance range and difference.

--- FRD-CV.py
+++ FRD-CV.py
@@ -5,22 +5,30 @@ import cv2 as cv
 import numpy as np
 import math
 
+# Image capture dimensions
 WIDTH, HEIGHT = 1920, 1088
 width, height = str(WIDTH), str(HEIGHT)
 
-scale = 0.25
+# Display image scaling
+scale = 0.5
 width_d = int(WIDTH * scale)
 height_d = int(HEIGHT * scale)
 dim = (width_d, height_d)
 
-MARKER_LENGTH_IN = 4.5 / 25.4 # "5mm Marker"
+# Marker length / scaling
+##MARKER_LENGTH_IN = 4.5 / 25.4 # "5mm Marker"
+MARKER_LENGTH_IN = 0.3 # BS Value
+print("MARKER_LENGTH_IN = ", MARKER_LENGTH_IN)
 
+# Import Aruco marker dictionary
 arucoDict = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_100)
 
+# Import camera calibration data
 KD = np.load('CV_CameraCalibrationData.npz')
 K = KD['k']
 DIST = KD['dist']
 
+# Calculate 3D distance between markers
 def get_dist(img, ids, corners, newCamMtx):
     
     # Get rotation and translation vectors with respect to marker
@@ -31,26 +39,27 @@ def get_dist(img, ids, corners, newCamMtx):
         distCoeffs=0
         )
 
-    print("tvecs: ", tvecs)
-
+    # Extract translation vectors for each marker
     tvec0 = tvecs[0][0]
     tvec1 = tvecs[1][0]
-    print("tvec0: ", tvec0)
-    print("tvec1: ", tvec1)
 
+    # Calculate distance between markers
     distance = math.sqrt((tvec0[0] - tvec1[0]) ** 2 +
                          (tvec0[1] - tvec1[1]) ** 2 + 
                          (tvec0[2] - tvec1[2]) ** 2
                          )
 
+    # Draw marker borders & axes on display image
     cv.aruco.drawDetectedMarkers(image=img,
                                  corners=corners,
                                  ids=ids,
                                  borderColor=(0, 0, 255)
                                  )
 
+    # Resize display image
     disp_img = cv.resize(img, dim, interpolation=cv.INTER_AREA)
 
+    # Return calculated distance and marked display image
     return distance, disp_img
 
 
@@ -61,9 +70,11 @@ if __name__ == '__main__':
     camera.exposure_mode = 'sports'
     rawCapture = PiRGBArray(camera, size=(WIDTH, HEIGHT))
 
+    # Initialize stage statuses
     gotMin = False
     gotMax = False
 
+    # For each frame...
     for frame in camera.capture_continuous(rawCapture,
                                            format="bgr",
                                            use_video_port=True
@@ -93,10 +104,11 @@ if __name__ == '__main__':
                                                  cameraMatrix=K,
                                                  distCoeff=DIST
                                                  )
-        
+
+        # Initialize detected marker count
         count = 0
 
-        # Get maximum control distance
+        # Get maximum control distance (Stage 2)
         if gotMin == True:
             if gotMax == False:
                 if ids is not None:
@@ -104,6 +116,7 @@ if __name__ == '__main__':
                         count = count + 1
                         print(count, " tag detected (max)")
                         
+                # When two markers are detected, get distance and show image                        
                 if count >= 2:
                     maxDist, maxImg = get_dist(img, ids, corners, newCamMtx)
                     print("Maximum Distance: ", maxDist)
@@ -111,14 +124,22 @@ if __name__ == '__main__':
 
                     cv.imshow("Max. Control Img", maxImg)
                     cv.waitKey(0)
+                    
+                    # Print calibration distance range and difference
+                    print("Control Distance Range: ",
+                          round(minDist, 4), " - ",
+                          round(maxDist, 4), " inches")
+                    print("Control Distance Difference : ",
+                          round(maxDist - minDist, 4), " inches")
 
-        # Get minimum control distance
+        # Get minimum control distance (Stage 1)
         if gotMin == False:
             if ids is not None:
                 for tag in ids:
                     count = count + 1
                     print(count, " tag detected (min)")
                     
+            # When two markers are detected, get distance and show image        
             if count >= 2:
                 minDist, minImg = get_dist(img, ids, corners, newCamMtx)
                 print("Minimum Distance: ", minDist)
@@ -128,21 +149,10 @@ if __name__ == '__main__':
                 cv.waitKey(0)
                     
 
-        # Get live output values
+        # Resize and show live stream feed
         resized = cv.resize(img, dim, interpolation = cv.INTER_AREA)
             
-        cv.imshow("mainstream", resized)
+        cv.imshow("Stream", resized)
         cv.waitKey(1)
+        
         rawCapture.truncate(0)
-
-        corners, ids, _ = cv.aruco.detectMarkers(image=img, dictionary=arucoDict, cameraMatrix=K, distCoeff=DIST)
-
-        if ids is None:
-            print("Marker not detected.")
-
-        if ids is not None:
-            for tag in ids:
-                cv.aruco.drawDetectedMarkers(image=img, corners=corners, ids=ids, borderColor=(0, 0, 255))
-                resized2 = cv.resize(img, dim, interpolation = cv.INTER_AREA)
-                cv.imshow("Stream - DETECTED", resized2)
-                cv.waitKey(1)
